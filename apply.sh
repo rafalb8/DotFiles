bbf169099f73b69a2b2210d8125f673fea645288
Rafalb8 <rafalb8@hotmail.com>
2022-07-09 19:42:11 +0200

    Remove custom apply patch funtion

--- apply.sh
+++ apply.sh
@@ -39,42 +39,9 @@ def getDistro():
             if line.startswith("ID"):
                 return line.split("=")[1].strip()
 
-def apply_patch(s,patch,revert=False):
-    """
-    Apply unified diff patch to string s to recover newer string.
-    If revert is True, treat s as the newer string, recover older string.
-    https://stackoverflow.com/a/40967337
-    """
-    s = s.splitlines(True)
-    p = patch.splitlines(True)
-    t = ''
-    i = sl = 0
-    (midx,sign) = (1,'+') if not revert else (3,'-')
+def apply_patch(file):
+    return os.popen("patch -p1 {0} {0}.patch --output -".format(file))
 
-    # skip header lines
-    while i < len(p) and p[i].startswith(("---","+++","diff", "index")):
-        i += 1 
-
-    while i < len(p):
-        m = _hdr_pat.match(p[i])
-        if not m:
-            raise Exception("Cannot process diff")
-        i += 1
-        l = int(m.group(midx))-1 + (m.group(midx+1) == '0')
-        t += ''.join(s[sl:l])
-        sl = l
-
-        while i < len(p) and p[i][0] != '@':
-            if i+1 < len(p) and p[i+1][0] == '\\':
-                line = p[i][:-1]; i += 2
-            else:
-                line = p[i]; i += 1
-            if len(line) > 0:
-                if line[0] == sign or line[0] == ' ':
-                    t += line[1:]
-                sl += (line[0] != sign)
-        t += ''.join(s[sl:])
-    return t
 
 def installPackages():
     # create list of packages to install
@@ -141,14 +108,16 @@ def apply():
     for file, locations in dotFiles.items():
         for path in locations:
             path = path.replace("~", os.environ["HOME"])
-
-            # read dot file
-            with open(file) as f:
-                dotfile = f.read()
             
+            dotfile = ""
+
             # apply patch
             if os.path.exists(file + ".patch"):
-                dotfile = apply_patch(dotfile, open(file + ".patch").read())
+                dotfile = apply_patch(file)
+            else:
+                # read dot file
+                with open(file) as f:
+                    dotfile = f.read()
             
             # compare dotfile with file from path
             with open(path) as f:
